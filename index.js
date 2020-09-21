d5750e5c9dca918427d63a68969dc4059e626a67
Vinayan <vinayankajith@gmail.com>
2020-09-21 15:31:59 +0530

    delete request added,seesion added,middleware added

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express= require('express')
 const dataservice= require('./services/dataservice')
 const app= express();
+const session=require('express-session');
 
+app.use(session({
+    secret: 'randomsecurestring',
+    resave:false,
+    saveUninitialized:false
+}));
 app.use(express.json())
+const logMiddlewear=(req,res,next) =>{
+    console.log(req.body)
+    next();
+
+}
+app.use(logMiddlewear);
+const authMiddlewear= (req,res,next)=>{
+    if(!req.session.currentUser)
+    {
+        return res.json({
+            status:false,
+            statusCode:401,
+            message:"Not authenticated please login",
+        
+          })
+  
+    }else{
+        next();
+    }
+}
 
 
 app.get('/',(req,res)=>{
@@ -14,9 +40,31 @@ app.post('/register',(req,res)=>{
      res.status(result.statusCode).json(result);
 })
 app.post('/login',(req,res)=>{
-    const result=dataservice.login(req.body.acno1,req.body.pwd)
+    const result=dataservice.login(req,req.body.acno1,req.body.pwd)
    res.status(result.statusCode).json(result);
 })
+app.post('/deposit', authMiddlewear,(req,res)=>{
+    //console.log(req.session.currentUser)
+    const result=dataservice.deposit(req.body.acno,req.body.pin,req.body.amt)
+   res.status(result.statusCode).json(result);
+
+})
+app.post('/withdraw', authMiddlewear,(req,res)=>{
+    const result=dataservice.withdraw(req.body.acno,req.body.pin,req.body.amt)
+   res.status(result.statusCode).json(result);
+
+})
+app.get('/transactionHistory', authMiddlewear,(req,res)=>{
+const result=dataservice.getTransactionDetails(req)
+res.status(200).json(result);
+})
+app.delete('/transactionHistory/:id', authMiddlewear,(req,res)=>{
+    //console.log(req.params.id)
+    const result=dataservice.deleteTansaction(req,req.params.id)
+     res.status(200).json(result);
+})
+
+
 app.put('/',(req,res)=>{
 
     res.send("put methd");
--- services/dataservice.js
+++ services/dataservice.js
@@ -1,67 +1,219 @@
 accountDetails = {
 
-    1001: { name: "user1", acno: 1001, pin: 4387, password: "userone", balance: 3000, transactions: [] },
-    1002: { name: "user2", acno: 1002, pin: 1234, password: "usertwo", balance: 3000, transactions: [] },
-    1003: { name: "user3", acno: 1003, pin: 4567, password: "userthree", balance: 3000, transactions: [] },
-    1004: { name: "user4", acno: 1004, pin: 5678, password: "userfour", balance: 3000, transactions: [] },
-    1005: { name: "user5", acno: 1005, pin: 6789, password: "userfive", balance: 3000, transactions: [] }
+  1001: { name: "user1", acno: 1001, pin: 4387, password: "userone", balance: 3000, transactions: [] },
+  1002: { name: "user2", acno: 1002, pin: 1234, password: "usertwo", balance: 3000, transactions: [] },
+  1003: { name: "user3", acno: 1003, pin: 4567, password: "userthree", balance: 3000, transactions: [] },
+  1004: { name: "user4", acno: 1004, pin: 5678, password: "userfour", balance: 3000, transactions: [] },
+  1005: { name: "user5", acno: 1005, pin: 6789, password: "userfive", balance: 3000, transactions: [] }
 
 }
+let currentUser
 const register = (name, acno, pin, pwd) => {
-    if (acno in accountDetails) {
+  if (acno in accountDetails) {
 
-        return {
-            result: false,
-            statusCode:422,
-            message: "Account already exists"
-        }
-    }
-    accountDetails[acno] = {
-        name,
-        acno,
-        pin,
-        password: pwd,
-        balance: 0,
-        transactions: []
-
-
-    }
     return {
-        result: true,
-        statusCode:200,
-        message: "Account created Please login"
+      result: false,
+      statusCode: 422,
+      message: "Account already exists"
+    }
+  }
+  accountDetails[acno] = {
+    name,
+    acno,
+    pin,
+    password: pwd,
+    balance: 0,
+    transactions: []
+
+
+  }
+  return {
+    result: true,
+    statusCode: 200,
+    message: "Account created Please login"
+  }
+}
+const login = (req, acno1, pwd) => {
+  //console.log(abc.value);
+  //console.log(defg.value)// Template Referencing
+  var acno = parseInt(acno1);
+
+  var data = accountDetails;
+  //console.log(acno in data);
+  req.session.currentUser = data[acno];
+  console.log(req.session.currentUser)
+  if (acno in data) {
+    let pd = data[acno].password
+    //console.log(pd);
+    if (pd == pwd) {
+      //this.currentUser = data[acno];
+      //this.setDetails();
+      return {
+        status: true,
+        statusCode: 200,
+        message: "Login Success"
+      }
+
+    }
+  }
+  return {
+    status: false,
+    statusCode: 422,
+    message: "Invalid credentials"
+  }
+}
+
+
+const deposit = (acno1, pp, amt) => {
+
+  
+  var acc = parseInt(acno1);
+  //var pp=this.dashboardForm.value.pin;
+  //var amt = parseInt(amd);
+  console.log(amt);
+
+  var details = accountDetails
+
+  if (acc in details) {
+
+
+    //console.log(acc);
+    let mpin = details[acc].pin;
+    //console.log(mpin);
+    //var bal = details[acc].balance;
+    // console.log(bal);
+    //var bala = parseInt(bal);
+    // console.log(bala);
+    if (pp == mpin) {
+      details[acc].balance += parseInt(amt);
+      details[acc].transactions.push({
+
+        Amount: amt,
+        Type: "Credit",
+        Accountnum:acc,
+        id:Math.floor(Math.random()*10000)
+
+      })
+      //console.log(details[acc].transactions)
+
+
+
+      //console.log(res);
+      //this.setDetails();
+
+      return {
+        status: true,
+        statusCode: 200,
+        message: "Amount has been credited",
+        Balance: details[acc].balance
+      }
+
+    }
+
+
+  }
+  else
+    return {
+      status: false,
+      statusCode: 422,
+      message: "Invalid credentials"
+
     }
 }
-const login = (acno1, pwd) => {
-    //console.log(abc.value);
-    //console.log(defg.value)// Template Referencing
-    var acno = parseInt(acno1);
 
-    var data = accountDetails;
-    console.log(acno in data);
-    if (acno in data) {
-        let pd = data[acno].password
-        console.log(pd);
-        if (pd == pwd) {
-            //this.currentUser = data[acno];
-            //this.setDetails();
-            return {
-                status: true,
-                statusCode:200,
-                message: "Login Success"
-            }
 
-        }
-    }
-    return {
+const withdraw = (acno1, pi, amt) => {
+  var acnt = parseInt(acno1);
+  //var pi=this.dashboardForm.value.pin;
+  var amd = parseInt(amt)
+
+  let db = accountDetails;
+
+  if (acnt in db) {
+    //console.log(acnt);
+
+    let wpin = db[acnt].pin;
+    if (db[acnt].balance < amd)
+      return {
         status: false,
-        statusCode:422,
-        message: "Invalid credentials"
+        statusCode: 422,
+        message: "Insufficient Balance",
+        Balance: db[acnt].balance
+
+      }
+
+    if (pi == wpin) {
+      db[acnt].balance -= amd;
+      db[acnt].transactions.push({
+        Amount: amd,
+        Type: "debit",
+        Accountnum:acnt,
+        id:Math.floor(Math.random()*10000)
+      })
+      //console.log(db[acnt].transactions)
+
+      //this.currentUser = db[acnt].balance;
+      //this.setDetails();
+      return {
+        status: true,
+        statusCode: 200,
+        message: "Amount has been debited",
+        Balance: db[acnt].balance
+      }
+
     }
+    else {
+      return {
+        staus: false,
+        statusCode: 422,
+        message: "insufficent  balance"
+
+      }
+    }
+  }
 }
+const getTransactionDetails = (req) => {
+  console.log(req.session.currentUser.acno)
+  return accountDetails[req.session.currentUser.acno].transactions;
+
+
+
+}
+const deleteTansaction=(req,id) =>{
+let transactions= accountDetails[req.session.currentUser.acno].transactions;
+transactions=transactions.filter(t=>{
+
+  if(t.id==id)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+})
+accountDetails[req.session.currentUser.acno].transactions=transactions;
+return {
+  staus: true,
+  statusCode: 200,
+  message: "Transaction history deleted"
+
+}
+
+}
+
+
+
+
+
+
 
 
 module.exports = {
-    register,
-    login
+  register,
+  login,
+  deposit,
+  withdraw,
+  getTransactionDetails,
+  deleteTansaction
 }
